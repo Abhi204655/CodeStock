abbfec635d9842bd3bbaf87792558bf8da551e90
Abhi204655 <Abhi9780253712@gmail.com>
2020-06-30 18:05:21 +0530

    configured * route

--- server.js
+++ server.js
@@ -60,6 +60,9 @@ const port = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendfile(path.join(__dirname + '/client/public/index.html'));
+    })
 }
 
 
