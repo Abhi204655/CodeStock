fc27d9768a81a24cbeb24fa63e78f7cd209ce99e
Abhi204655 <Abhi9780253712@gmail.com>
2020-07-03 15:51:12 +0530

    removed redux dev tools

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -8,9 +8,7 @@ const initialState = {}
 const middlewares = [thunk]
 
 const store = createStore(rootReducer, initialState, compose(
-    applyMiddleware(...middlewares),
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(...middlewares)
 ))
 
 export default store;
